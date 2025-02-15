e1c81fb0964f95720febf8f81bf3eb55e8b716b3
Zulusar <150274779+Zulusar@users.noreply.github.com>
2025-02-15 21:26:55 +0500

    qwerty

--- src/1/1_1_4/App.tsx
+++ src/1/1_1_4/App.tsx
@@ -2,3 +2,20 @@
 /* 
 Напишите компонент с нуля. Вы можете дать ему любое допустимое имя и вернуть любую разметку. Если у вас нет идей, вы можете написать компонент Congratulations, который показывает <h1>Хорошая работа!</h1>. Не забудьте экспортировать это!
 */
+function Congratulations(){
+    return(
+    <img
+    src = "img_16.11.2024.22.30.jpg"
+    alt = "Funny cat"
+    />
+    )
+}
+
+export default function MyFirstCommit(){
+    return(
+        <section>
+            <h1>Oh yeah! That's great!</h1>
+            <Congratulations />
+        </section>
+    )
+}
